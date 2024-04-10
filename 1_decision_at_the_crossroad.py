c24cd477627adb847c5bc562566f263139a2f438
Kayla Fernando <165612337+KDanyelle@users.noreply.github.com>
2024-04-10 12:44:57 -0400

    Add files via upload

---
+++ 1_decision_at_the_crossroad.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: 2"))
+if number > 0:
+    print("The number is positive.")
+elif number == 0:
+    print("The number is 0.")
+else:
+    print("The number is negative.")
+
---
+++ 2_the_greatest_showdown.py
@@ -0,0 +1,25 @@
+#Task 1 greatest
+
+num1 = input("Enter first number:4")
+num2 = input("Enter second number:3")
+num3 = input("Enter third number:1")
+if num1 > greatest:
+    greatest = 4
+if num3 < greatest:
+    greatest = 3
+    print("The greatest number is:", greatest)
+
+
+#Task 2 smallest 
+
+num1 = input("Enter first number:1")
+num2 = input("Enter second number:3")
+num3 = input("Enter third number: 4")  
+smallest = num1
+if num1 < smallest:
+    smallest = num1
+if num1 < smallest:
+    smallest = num1
+    print("The smallest number is:",smallest)
+
+
